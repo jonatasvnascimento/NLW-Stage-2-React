b723eb3f299dc3da23343d878bff210c7393a59b
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2021-10-25 20:49:33 -0300

    Finish layout

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -5,10 +5,51 @@ export function MessageList() {
   return (
     <div className={styles.messageListWrapper}>
       <img src={logoImg} alt="DoWhile 2021" />
-
       <ul className={styles.messageList}>
         <li className={styles.message}>
-          <p>Teste</p>
+          <p className={styles.messageContent}>
+            Nessa aula criaremos o front-end web da nossa aplicação utilizando
+            ReactJS. Além disso, criaremos nosso projeto utilizando o Vite
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/jonatasvnascimento.png"
+                alt="Jonatas Viana"
+              />
+            </div>
+            <span>Jonatas Viana</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Nessa aula criaremos o front-end web da nossa aplicação utilizando
+            ReactJS. Além disso, criaremos nosso projeto utilizando o Vite
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/jonatasvnascimento.png"
+                alt="Jonatas Viana"
+              />
+            </div>
+            <span>Jonatas Viana</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Nessa aula criaremos o front-end web da nossa aplicação utilizando
+            ReactJS. Além disso, criaremos nosso projeto utilizando o Vite
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/jonatasvnascimento.png"
+                alt="Jonatas Viana"
+              />
+            </div>
+            <span>Jonatas Viana</span>
+          </div>
         </li>
       </ul>
     </div>
